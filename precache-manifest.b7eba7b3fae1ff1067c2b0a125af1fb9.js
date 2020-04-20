d68653b7ed2eb0db9fe9cb8f7f6fbaa91de85271
Anil Suryaprakash <anilkutsa@gmail.com>
2020-04-20 17:03:36 +0530

    Updates

---
+++ precache-manifest.b7eba7b3fae1ff1067c2b0a125af1fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f69a3bb019b2b4f0a9cf16f30ea1a3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a2cbc894e666e198ae80",
+    "url": "/monster-rolodex/static/css/main.40c75cbb.chunk.css"
+  },
+  {
+    "revision": "d449f139d4604a7080f2",
+    "url": "/monster-rolodex/static/js/2.084dfeb5.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monster-rolodex/static/js/2.084dfeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2cbc894e666e198ae80",
+    "url": "/monster-rolodex/static/js/main.0138e76c.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
